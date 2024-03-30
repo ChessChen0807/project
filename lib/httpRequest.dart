c18efacc12de5163474cdafd2fee4e43b92beb80
Anjun Chen <anjunchen@anjundembp.lan>
2024-03-30 11:33:40 -0700

    Almost finished chatbot

--- lib/httpRequest.dart
+++ lib/httpRequest.dart
@@ -9,25 +9,44 @@ class HttpPage extends StatefulWidget {
 class _HttpPageState extends State<HttpPage> {
 
   String _responseData = "";
+  TextEditingController _textController = TextEditingController();
+  List<ChatMessage> _message = <ChatMessage>[];
+
+  void _handleSubmitted(String text){
+    if(_textController.text.isNotEmpty){
+      _textController.clear();
+
+      ChatMessage message = ChatMessage(message: text,
+          isUserMessage: true
+      );
+      setState(() {
+        _messages.insert(0, message);
+      });
+      fetchData('get_opening_advice/$text');
+    }
+  }
 
   //Function for fetching data from our api
   Future<void> fetchData(String path) async {
-  //declare url
-    var url = Uri.parse('https://chess-opening-analyzer.onrender.com');
+    //declare url
+    var url = Uri.parse('https://chess-opening-analyzer.onrender.com/$path');
 
     try {
       // http GET request
       var response = await http.get(url);
 
       //check if response was successful
-      if (response.statusCode == 200){
-        setState((){
+      if (response.statusCode == 200) {
+        ChatMessage message = ChatMessage(message: response.body, isUserMessage: false)
+
+        setState(() {
           _responseData = response.body;
+          _message.insert(0, message);
         });
       } else {
         print("Request failed with status: ${response.statusCode}");
       }
-    }catch (e) {
+    } catch (e) {
       print("error: $e");
     }
   }
@@ -35,14 +54,89 @@ class _HttpPageState extends State<HttpPage> {
   @override
   @override
   Widget build(BuildContext context) {
-    fetchData('/get_opening_name'); // test to run function
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HTTP Request Example'),
+        appBar: AppBar(
+          title: Text('Chess GPT'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Flexible(
+                child: ListView.builder(
+                  itemBuilder: (_, int index) => _message[index],
+                  itemCount: _message.length,
+                ),
+            ),
+          ],
+        ));
+  }
+
+  Widget _buildTextComposer(){
+    return IconTheme(
+        data: IconThemeData(),
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                child: Flexible(
+                  child: TextField(
+                  controller: _textController,
+                     onSubmitted: _handleSubmitted,
+                    decoration: InputDecoration.collapsed(hintText: 'Send a message'),
+          ),
+                ),
+              ),
+              Containder(
+                margin: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: IconButton(
+                      icon: Icon(Icons.send),
+                      onPressed: () => _handleSubmitted(_textController.text),
+                    )
+              )
+            ],
+          ),
+        )),
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  final String message;
+  final bool isUserMessage;
+
+  ChatMessage({required this.message, required this.isUserMessage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            children: [
+              Column(
+                crossAxisAlignment: isUserMessage ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    isUserMessage ? 'Me' : 'Chess GPT',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 5.0),
+                    padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: isUserMessage
+                          ? Colors.blue.withOpacity(0.3)
+                          : Colors.grey.withOpacity(0.3),
+                    ),
+                    child: Text(message),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
       ),
-      body: Center(
-        child: Text(_responseData) //display response from api
-      )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'landing_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'ApplicationState.dart';
+import 'httpRequest.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,25 +26,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LandingPage(name: "Login / Sign Up"),
+      home: AuthenticationWrapper()
+    );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return CircularProgressIndicator();
+          }else{
+            if(snapshot.hasData){
+              print("User is logged in");
+              return HttpPage();
+            }else{
+              print("User needs to log in");
+              return LandingPage(name: 'Login / Sign Up');
+            }
+          }
+        }
     );
   }
 }
